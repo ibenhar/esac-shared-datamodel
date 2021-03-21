0e8f1190810cb7df77b45d38fd905d64cbb2de46
ibenhar <ilias.benhar@sap.com>
2021-03-21 19:54:44 +0100

    progress

--- categoryTest.js
+++ categoryTest.js
@@ -1,1 +1,24 @@
-iliassou
+import mongoose, { Schema } from 'mongoose'
+
+export const CategorySchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    visibleOnline: {
+        type: Boolean,
+        default: false
+    },
+    productsCount: {
+        type: String
+    },
+    products: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Product'
+        
+    }]
+    
+}, {
+    timestamps: true
+});
