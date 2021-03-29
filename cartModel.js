9c8c46fd000b7c7b7b55604b9b45d18b1aca0080
ibenhar <ilias.benhar@sap.com>
2021-03-29 22:10:03 +0100

    progress

---
+++ cartModel.js
@@ -0,0 +1,116 @@
+import mongoose, { Schema } from 'mongoose'
+
+
+const CartEntrySchema = new Schema({ 
+    code: {
+        type: String,
+        required: true
+    }, 
+    totalPrice: {
+        type: Number,
+        required: true
+    }, 
+    qty: {
+        type: Number,
+        required: true
+    },    
+    productMainImgUrl: {
+        type: String,
+        required: false
+    }, 
+    productCode: {
+        type: String,
+        required: true
+    }, 
+    productName: {
+        type: String,
+        required: true
+    }, 
+    basePrice: {
+        type: Number,
+        required: true
+    }, 
+    promo: {
+        type: Boolean,
+        required: false
+    },
+    discountValue: {
+        type: Number,
+        required: false
+    }, 
+    discountPercentage: {
+        type: Boolean,
+        required: false
+    }, 
+    productReferencePrice: {
+        type: Number,
+        required: false
+    }
+});
+
+export const CartSchema = new Schema({
+
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+    deliveryCost: {
+        type: String,
+        required: true
+    },
+    deliveryMode: {
+        type: String,
+        required: true
+    },
+
+    customerEmail: {
+        type: String
+    },
+
+    customerPhone: {
+        type: String,
+        required: true
+    },
+
+    customer: {
+        type: Schema.Types.ObjectId,
+        ref: 'Customer'
+    },
+
+    status: {
+        type: String,
+        required: true
+    },
+    paymentStatus: {
+        type: String,
+        required: true
+    },
+    deliveryAddress: {
+        type: String,
+        required: true
+    },
+
+    deliveryApartment: {
+        type: String
+    },
+
+    deliveryEnterprise: {
+        type: String
+    },
+
+    deliveryNote: {
+        type: String
+    },
+
+    pickupStore: {
+        type: String
+    },
+    entries: {
+        type: [CartEntrySchema],
+        default: []
+    }
+    
+}, {
+    timestamps: true
+});
+
